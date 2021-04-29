43778db00e5791bd297935a81d853b5a12efcc2d
ricknois <rickssti@outlook.com>
2021-04-28 22:44:13 -0300

    :sparkles: Initial setup and api fetch

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import axios from 'axios';
 
 export default function App() {
+  const [names, setNames] = useState([]);
+
+  const fetchNames = () => {
+     axios
+     .get('https://randomuser.me/api/?nat=br&results=5')
+     .then(response => {
+       const { results } = response.data;
+       setNames(results)
+     })
+  };
+  
+  useEffect(() => {
+    fetchNames();
+  }, [])
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={style.app}>
+      <Header title='Pessoas' />
       <StatusBar style="auto" />
+      {names.map((name, index) => <Text key={index}>{ name.name.first }</Text>)}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const style = StyleSheet.create({
+  app: {
+    backgroundColor: 'white',
+  }
 });
